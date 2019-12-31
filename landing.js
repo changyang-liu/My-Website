324142a6f88b9a3ec47c128ebb5eb5fe3c259a27
liuchangyang <liuchangyang112@gmail.com>
2019-12-31 19:24:25 +0800

    Import from local folder. Source code and assets.

---
+++ landing.js
@@ -0,0 +1,19 @@
+$(window).scroll(() => {
+    let scroll = $(window).scrollTop();
+    //Toggle arrow visibility
+    if(scroll >= 30){
+        $('#arrow').css('display', 'none');
+        //Toggle navbar color
+        if(scroll < 100){
+            $('.navbar').css('background', 'transparent');
+            $('.navbar').removeClass('navbar-dark');
+            $('.navbar').addClass('navbar-light');
+        }else{
+            $('.fixed-top').css('background', '#756a64');
+            $('.navbar').removeClass('navbar-light');
+            $('.navbar').addClass('navbar-dark');
+        }
+    }else{
+        $('#arrow').css('display', 'inline-block')
+    }
+});
